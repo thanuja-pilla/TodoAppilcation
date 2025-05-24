f8d79dcd623b09840053c9d178dba86e1e721434
Thanuja Pilla <thanujapilla7@gmail.com>
2025-05-24 14:33:20 +0000

    added remove functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-todos = [];
+let todos = [];
 
+// eslint-disable-next-line no-unused-vars
 function addTodo() {
     const input = document.getElementById('todo-input');
     if(input.value.trim() !== '') {
@@ -9,12 +10,28 @@ function addTodo() {
     }
 }
 
+function removeTodo(index) {
+    todos.splice(index,1);
+    renderTodos();
+}
+
+
 function renderTodos() {
     const list = document.getElementById('todo-list');
     list.innerHTML = '';
-    todos.forEach((todo,indexl) => {
+    todos.forEach((todo,index) => {
         const listItem = document.createElement('li');
         listItem.textContent = todo;
+
+        const removeBtn = document.createElement('button');
+        removeBtn.textContent = 'Delete';
+        removeBtn.style.marginLeft = '10px';
+        removeBtn.style.color = 'red';
+        removeBtn.onclick = function() {
+            removeTodo(index);
+        };
+        listItem.appendChild(removeBtn);
+
         list.appendChild(listItem);
 
     });
